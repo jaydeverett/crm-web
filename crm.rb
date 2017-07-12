0ca0d1747b657424b657d19246dd98b8f89bad76
Jay Deverett <jayjdeverett@gmail.com>
2017-07-12 17:47:38 -0400

    complete Part 1

---
+++ crm.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+require_relative 'contact'
+
+get '/' do
+  erb :index
+end
+
+get '/contacts' do
+  @contacts = Contact.all
+  erb :contacts
+end
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
