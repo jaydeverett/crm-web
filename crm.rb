dd250a4c2c0facb301706ab484d60e9d61940b29
Jay Deverett <jayjdeverett@gmail.com>
2017-07-13 18:23:06 -0400

    add navigation to crm

--- crm.rb
+++ crm.rb
@@ -11,6 +11,15 @@ get '/contacts' do
   erb :contacts
 end
 
+get '/contacts/:id' do
+  contact_id = params[:id]
+  @contact = Contact.find_by({id: params[:id].to_i})
+  if @contact
+  erb :show_contact
+else
+  raise Sinatra::NotFound
+end
+end
 
 after do
   ActiveRecord::Base.connection.close
