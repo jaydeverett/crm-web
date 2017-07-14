3a2ed88178335e3111ad20a100a15cd788cd92a2
Jay Deverett <jayjdeverett@gmail.com>
2017-07-14 13:43:43 -0400

    add contact create params and redirect

--- crm.rb
+++ crm.rb
@@ -25,6 +25,16 @@ else
 end
 end
 
+post '/contacts' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/contacts')
+end
+
 after do
   ActiveRecord::Base.connection.close
 end
